b395723b2d4b1115d270b4e39b48001b419e6815
Ronald Huggler <ronald@huggler.com.br>
2018-02-20 14:12:10 -0300

    Set PostgreSQL as default database

--- django_crud/django_crud/settings.py
+++ django_crud/django_crud/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'django_crud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
