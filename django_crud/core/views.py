ecc9cadfe8fb7d5a2006cd7bb37a44077aebc869
Ronald Huggler <ronald@huggler.com.br>
2018-02-21 15:14:19 -0300

    Add to-do list/create view

--- django_crud/core/views.py
+++ django_crud/core/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import Http404
 
-# Create your views here.
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class TodoList(APIView):
+    """
+    List all To-Dos or create a new one.
+    """
+    def get(self, request, format=None):
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
