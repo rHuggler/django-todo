b186af7ab42ff0baed1e307feca1133c9c54d345
Ronald Huggler <ronald@huggler.com.br>
2018-02-21 13:04:19 -0300

    Add models

--- django_crud/core/models.py
+++ django_crud/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils.text import Truncator
 
-# Create your models here.
+class Todo(models.Model):
+    text = models.CharField(max_length=50, blank=False)
+    status = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True)
+    last_modified = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return Truncator(self.message).chars(20)
