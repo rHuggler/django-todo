88886ab17d9b681cdd9bf60fab12e3bcd33837fa
Ronald Huggler <ronald@huggler.com.br>
2018-02-21 19:20:11 -0300

    Add to-do delete view

--- django_crud/core/views.py
+++ django_crud/core/views.py
@@ -41,3 +41,8 @@ class TodoItem(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        todo = get_object_or_404(Todo, id=pk)
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
