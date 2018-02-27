363e716d9677c781ec81dce8c5663392f4b15354
Ronald Huggler <ronald@huggler.com.br>
2018-02-27 12:26:38 -0300

    Change Date fields to DateTime

--- api/django_todo/core/models.py
+++ api/django_todo/core/models.py
@@ -4,8 +4,8 @@ from django.utils.text import Truncator
 class Todo(models.Model):
     text = models.CharField(max_length=50, blank=False)
     status = models.BooleanField(default=False)
-    created_at = models.DateField(auto_now_add=True)
-    last_modified = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return Truncator(self.message).chars(20)
